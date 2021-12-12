9ae867e8b6386bb5013a7702514f12e820ad1a40
SHELA <sheludko@vladyslav.ru>
2021-12-13 00:28:52 +0200

    Update story.py

--- instagrapi/story.py
+++ instagrapi/story.py
@@ -9,10 +9,10 @@ try:
 except ImportError:
     raise Exception("Please install moviepy==1.0.3 and retry")
 
-try:
-    from PIL import Image
-except ImportError:
-    raise Exception("You don't have PIL installed. Please install PIL or Pillow>=8.1.1")
+# try:
+#     from PIL import Image
+# except ImportError:
+#     raise Exception("You don't have PIL installed. Please install PIL or Pillow>=8.1.1")
 
 
 class StoryBuilder:
